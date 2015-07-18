72b08ef961d5870c91f77190d52a928914de87ce
mythirdeye <johntendik@hotmail.com>
2015-07-17 22:49:19 -0400

    Changed backend styles
    Included a tabs layout for the backend settings page.

--- jtrt-tables.php
+++ jtrt-tables.php
@@ -51,62 +51,76 @@ function jtrt_tables_plugin_options_page(){
 
 		<h2>JT Responsive Tables</h2>
 
-		<p>Step 1: Upload a CSV File using the file uploader or link one from an external source. You must save the form, (Click save changes button) before the link can be registered in the script</p>
-		<p>Step 2: Once the link is saved, press the "Generate Table" button</p>
-		<p>Step 3: Click on the Header columns to select them, the rows will highlight blue. The blue rows will be hidden when the form is tablet sized.</p>
-		<p>Step 4: Once you select the rows that you want to hide on mobile sizes, press the generate HTML link and copy paste the HTML from the textbox into your post.</p>
-		<p>NOTE: You should have a maximum of 4 columns visible for the mobile sizes to work properly, all other columns should be hidden. Otherwise the table will be too large and flow off the screen.(ie. If you have 12 columns, at least 8 of those should be hidden.)</p>
+		<div id="tabs">
+		    <ul>
+		        <li><a href="#fragment-1"><span>Docs</span></a></li>
+		        <li><a href="#fragment-2"><span>Table Settings</span></a></li>
+		        <li><a href="#fragment-3"><span>Table Generator</span></a></li>
+		    </ul>
+		    <div id="fragment-1">
+		        <h3>Documentation & Guides</h3>
+		        <p>Step 1: Upload a CSV File using the file uploader or link one from an external source. You must save the form, (Click save changes button) before the link can be registered in the script</p>
+		        <p>Step 2: Once the link is saved, press the "Generate Table" button</p>
+		        <p>Step 3: Click on the Header columns to select them, the rows will highlight blue. The blue rows will be hidden when the form is tablet sized.</p>
+		        <p>Step 4: Once you select the rows that you want to hide on mobile sizes, press the generate HTML link and copy paste the HTML from the textbox into your post.</p>
+		        <p>NOTE: You should have a maximum of 4 columns visible for the mobile sizes to work properly, all other columns should be hidden. Otherwise the table will be too large and flow off the screen.(ie. If you have 12 columns, at least 8 of those should be hidden.)</p>
+		    </div>
+		    <div id="fragment-2">
+		        <form method="post" action="options.php">
 
+	    			<?php settings_fields( 'jtrt-options-group' ); ?>
 
-		<form method="post" action="options.php">
+	    			<?php do_settings_sections( 'jtrt-options-group' ); ?>
 
-	    	<?php settings_fields( 'jtrt-options-group' ); ?>
+				   	<fieldset class="jtrt_fieldset">
 
-	    	<?php do_settings_sections( 'jtrt-options-group' ); ?>
+						<label for="upload_image"><b>CSV file</b></label></br>
+						<input id="upload_image" type="text" size="36" input type='text' name='jtrt_tables_options[kwrc_table_link]' value='<?php echo $jtrt_options['kwrc_table_link']; ?>'/> 
 
-		   	<fieldset class="jtrt_fieldset">
+						<input id="upload_image_button" class="button" type="button" value="Upload file" />
+						<small>Enter a URL or upload a CSV file</small>
 
-				<label for="upload_image"><b>CSV file</b></label></br>
-				<input id="upload_image" type="text" size="36" input type='text' name='jtrt_tables_options[kwrc_table_link]' value='<?php echo $jtrt_options['kwrc_table_link']; ?>'/> 
+						<br/>
 
-				<input id="upload_image_button" class="button" type="button" value="Upload file" />
-				<small>Enter a URL or upload a CSV file</small>
+					</fieldset>
 
-				<br/>
+					<fieldset class="jtrt_fieldset">
 
-			</fieldset>
+						<table class="form-table">
+							<tbody>
+								<tr>
+									<th scope="row">
+										<label for="foo_breakpoint_mobile"><b>Mobile Breakpoint</b></label></br>
+									</th>
 
-			<fieldset class="jtrt_fieldset">
+									<td>
+										<input type="text" id="foo_breakpoint_mobile" name='jtrt_tables_options[kwrc_table_foo_breakpoint_mobile]' value='<?php echo (empty($jtrt_options['kwrc_table_foo_breakpoint_mobile']) ? '480' : $jtrt_options['kwrc_table_foo_breakpoint_mobile']); ?>'>
+										<br>
+										<span class="description">The width of the phone breakpoint.</span>
+									</td>
+								</tr>
+								<tr>
+									<th scope="row">
+										<label for="foo_breakpoint_tablet"><b>Tablet Breakpoint</b></label></br>
+									</th>
 
-				<table class="form-table">
-					<tbody>
-						<tr>
-							<th scope="row">
-								<label for="foo_breakpoint_mobile"><b>Mobile Breakpoint</b></label></br>
-							</th>
-
-							<td>
-								<input type="text" id="foo_breakpoint_mobile" name='jtrt_tables_options[kwrc_table_foo_breakpoint_mobile]' value='<?php echo (empty($jtrt_options['kwrc_table_foo_breakpoint_mobile']) ? '480' : $jtrt_options['kwrc_table_foo_breakpoint_mobile']); ?>'>
-								<br>
-								<span class="description">The width of the tablet breakpoint.</span>
-							</td>
-						</tr>
-						<tr>
-							<th scope="row">
-								<label for="foo_breakpoint_tablet"><b>Tablet Breakpoint</b></label></br>
-							</th>
-
-							<td>
-								<input type="text" id="foo_breakpoint_tablet" name='jtrt_tables_options[kwrc_table_foo_breakpoint_tablet]' value='<?php echo (empty($jtrt_options['kwrc_table_foo_breakpoint_tablet']) ? '920' : $jtrt_options['kwrc_table_foo_breakpoint_tablet']); ?>'>
-								<br>
-								<span class="description">The width of the phone breakpoint.</span>
-							</td>
-						</tr>
-					</tbody>
-				</table>
-									
+									<td>
+										<input type="text" id="foo_breakpoint_tablet" name='jtrt_tables_options[kwrc_table_foo_breakpoint_tablet]' value='<?php echo (empty($jtrt_options['kwrc_table_foo_breakpoint_tablet']) ? '920' : $jtrt_options['kwrc_table_foo_breakpoint_tablet']); ?>'>
+										<br>
+										<span class="description">The width of the tablet breakpoint.</span>
+									</td>
+								</tr>
+							</tbody>
+						</table>				
 						
-			</fieldset>
+					</fieldset>
+
+					<?php submit_button(); ?>
+
+				</form>
+
+		    </div>
+		    <div id="fragment-3">
 
 			<fieldset class="jtrt_fieldset">
 
@@ -122,11 +136,24 @@ function jtrt_tables_plugin_options_page(){
 
 			</fieldset>
 
+		    </div>
+		</div>
+
+		
+
+		
+		
+		
+		
+		
+
+
+		
 
 	    
-	    	<?php submit_button(); ?>
+	    	
 
-		</form>
+		
 	</div>
 
 	<?php
@@ -140,10 +167,13 @@ function jtrt_tables_script_caller( $hook_suffix ) {
     }
 
 	wp_enqueue_media();
+	wp_enqueue_script('jquery-ui', 'https://code.jquery.com/ui/1.11.4/jquery-ui.min.js', array('jquery'));
 	wp_enqueue_script( 'table-generator-from-csv', plugins_url( 'jtrt-tables/js/jquery.csvToTable.js', dirname(__FILE__) ), array( 'jquery' ), 1, true );
 	wp_register_script( 'jtrt-csv-upload', plugins_url( 'jtrt-tables/js/jtrt-js-handler.js', dirname(__FILE__) ), array( 'jquery' ), 1, true );
 	wp_enqueue_script( 'jtrt-csv-upload' );
-	wp_enqueue_style( 'custom-style', plugins_url( 'jtrt-tables/jtrt-tables.css' ) );
+	wp_enqueue_style( 'jtrt-custom-style', plugins_url( 'jtrt-tables/jtrt-tables.css' ) );
+	wp_enqueue_style( 'jtrt-jquery-ui-css', 'https://code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css' );
+	
 
 }
 
